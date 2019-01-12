0cc72104a3510f4adc978e972f4d660aaf3996c5
itatsuya1250 <itatsuya1250@gmail.com>
2019-01-12 23:15:17 +0900

    ts

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ http.createServer(function (request, response) {
     response.end('Hello World\n');
 }).listen(3000);
 
-console.log('Server running at http://127.0.0.1:3000');
+console.log('Server running');
